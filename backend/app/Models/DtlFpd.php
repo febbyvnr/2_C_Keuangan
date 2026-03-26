1c852c6b6ca03fc3888ccf9ee2b40348975783b3
larasmarut2608 <larasmarut2608@gmail.com>
2026-03-26 16:00:06 +0700

    pemambahan code pada  DtlFpd

--- backend/app/Models/DtlFpd.php
+++ backend/app/Models/DtlFpd.php
@@ -3,8 +3,42 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DtlFpd extends Model
 {
-    //
+    protected $table = 'dtl_fpd';
+    protected $primaryKey = 'ID_DT_FPD';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ID_FPD',
+        'ID_DT_PROGKER',
+        'QTY',
+        'HARGA_SATUAN',
+        'VOLUME',
+        'SATUAN',
+        'TOTAL',
+        'LINK_BUKTI_NOTA_FPD',
+    ];
+
+    protected $casts = [
+        'ID_DT_FPD' => 'integer',
+        'ID_FPD' => 'integer',
+        'ID_DT_PROGKER' => 'integer',
+        'QTY' => 'integer',
+        'HARGA_SATUAN' => 'double',
+        'VOLUME' => 'integer',
+        'TOTAL' => 'double',
+    ];
+
+    public function fpd(): BelongsTo
+    {
+        return $this->belongsTo(FpdAnggaran::class, 'ID_FPD', 'ID_FPD');
+    }
+
+    public function detailProgram(): BelongsTo
+    {
+        return $this->belongsTo(DtlProgramKerja::class, 'ID_DT_PROGKER', 'ID_DT_PROGKER');
+    }
 }
