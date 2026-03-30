de420df90cf3af8808b2246dd6af854f45d8eb00
230712318 <230712318@students.uajy.ac.id>
2026-03-30 16:06:48 +0700

    mengisi file model RefJenisPembayaran

--- backend/app/Models/RefJenisPembayaran.php
+++ backend/app/Models/RefJenisPembayaran.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class RefJenisPembayaran extends Model
 {
-    //
+    protected $table = 'ref_jenis_pembayaran';
+    protected $primaryKey = 'id_jenis_pembayaran';
+    public $timestamps = false;
+
+    protected $fillable = ['deskripsi_jenis_pembayaran'];
 }
