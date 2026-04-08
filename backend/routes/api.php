63fedb2f08aa2a92782fb960009a604fe6ddf29d
GavrilaSrs12443 <230712443@students.uajy.ac.id>
2026-04-08 16:15:01 +0700

    create function export RKT, tapi belum ada template fixnya

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -103,7 +103,10 @@ Route::prefix('ref-tan')->group(function () {
 
 Route::prefix('rkt')->group(function () {
     Route::get('/', [MstProgramKerjaController::class, 'index']);
-    Route::get('/{id}', [MstProgramKerjaController::class, 'show']);
+
+    Route::get('/export/excel', [MstProgramKerjaController::class, 'exportExcel']);
+
+    Route::get('/{id}', [MstProgramKerjaController::class, 'show'])->whereNumber('id');
     Route::post('/store', [MstProgramKerjaController::class, 'store']);
     Route::put('/update/{id}', [MstProgramKerjaController::class, 'update']);
     Route::delete('/delete/{id}', [MstProgramKerjaController::class, 'destroy']);
