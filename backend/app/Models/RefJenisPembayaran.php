880e5f9814b5d2cca39867a5cc7480e487dc599a
230712318 <230712318@students.uajy.ac.id>
2026-04-09 13:45:56 +0700

    menambahkan id menjadi fillable

--- backend/app/Models/RefJenisPembayaran.php
+++ backend/app/Models/RefJenisPembayaran.php
@@ -9,6 +9,7 @@ class RefJenisPembayaran extends Model
     protected $table = 'ref_jenis_pembayaran';
     protected $primaryKey = 'id_jenis_pembayaran';
     public $timestamps = false;
+    protected $keyType = 'int';
 
-    protected $fillable = ['deskripsi_jenis_pembayaran'];
+    protected $fillable = ['deskripsi_jenis_pembayaran', 'id_jenis_pembayaran'];
 }
