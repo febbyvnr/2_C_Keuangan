0f32eecd64ee04ac77a4a51f41c7f4c1bcb67d1d
230712318 <230712318@students.uajy.ac.id>
2026-04-09 14:07:11 +0700

    mengedit typo

--- backend/app/Models/RefJenisPembayaran.php
+++ backend/app/Models/RefJenisPembayaran.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class RefJenisPembayaran extends Model
 {
     protected $table = 'ref_jenis_pembayaran';
-    protected $primaryKey = 'id_jenis_pembayaran';
+    protected $primaryKey = 'ID_JENIS_PEMBAYARAN';
     public $incrementing = false;
     public $timestamps = false;
     protected $keyType = 'int';
