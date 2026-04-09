db2823e6d4db61ee3b5fe68c21d38cdb73039606
230712318 <230712318@students.uajy.ac.id>
2026-04-09 13:54:43 +0700

    menukar posisi ID di fillable

--- backend/app/Models/RefJenisPembayaran.php
+++ backend/app/Models/RefJenisPembayaran.php
@@ -8,8 +8,9 @@ class RefJenisPembayaran extends Model
 {
     protected $table = 'ref_jenis_pembayaran';
     protected $primaryKey = 'id_jenis_pembayaran';
+    public $incrementing = false;
     public $timestamps = false;
     protected $keyType = 'int';
 
-    protected $fillable = ['deskripsi_jenis_pembayaran', 'id_jenis_pembayaran'];
+    protected $fillable = ['ID_JENIS_PEMBAYARAN','deskripsi_jenis_pembayaran'];
 }
