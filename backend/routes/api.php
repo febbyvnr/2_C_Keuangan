2ab6c8f56e486adf18eb314be556a0cc79e58437
GianTechWizard <giannkolebaa@gmail.com>
2026-04-14 00:06:47 +0700

    add route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\RefJenisPembayaranController;
 
 use Termwind\Components\Raw;
 use App\Http\Controllers\RkaController;
+use App\Http\Controllers\LaporanBukuKhasUmumController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -214,6 +215,11 @@ Route::prefix('tagihan-siswa')->group(function () {
 
 Route::prefix('laporan')->group(function () {
     Route::get('/penerimaan', [LaporanPenerimaanController::class, 'penerimaan']);
+    
+});
+
+Route::prefix('laporan')->group(function () {
+    Route::get('/penerimaan', [LaporanBukuKhasUmumController::class, 'penerimaan']);
 
 });
 
