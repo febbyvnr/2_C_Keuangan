d26046f28ec9e21f26d7468c0aa4b3a6698c612d
GianTechWizard <giannkolebaa@gmail.com>
2026-04-13 23:59:06 +0700

    add file sheetbkutunai

---
+++ backend/app/Exports/SheetBKUP1Tunai.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class SheetBKUP1Tunai extends SheetBKU
+{
+    public function registerEvents(): array
+    {
+        $events = parent::registerEvents();
+
+        $events[AfterSheet::class] = function ($event) {
+
+            // Panggil logic dari parent (SheetBKU)
+            parent::registerEvents()[AfterSheet::class]($event);
+
+            $sheet = $event->sheet;
+
+            // =====================
+            // UBAH JUDUL
+            // =====================
+            $sheet->setCellValue('A3', 'LAPORAN BUKU KAS UMUM - TUNAI (P1)');
+        };
+
+        return $events;
+    }
+}
