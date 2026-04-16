62581563c453b92002560beb87d16fb94a9201c0
Febby Venera <230712301@students.uajy.ac.id>
2026-04-16 21:05:51 +0700

    delete route persetujuan (tdk terpakai)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SidebarBendahara from "./components/SidebarBendahara";
 
 import Dashboard from "./pages/bendahara/Dashboard";
-import Persetujuan from "./pages/bendahara/Persetujuan";
-// import Persetujuan from "./pages/bendahara/Persetujuan";
 import Dana from "./pages/bendahara/Dana";
 import RKA from "./pages/bendahara/RKA";
 import BKU from "./pages/bendahara/BKU";
@@ -59,14 +57,6 @@ export default function App() {
           element={
             <BendaharaPage>
               <Dashboard />
-            </BendaharaPage>
-          }
-        />
-        <Route
-          path="/bendahara/persetujuan"
-          element={
-            <BendaharaPage>
-              <Persetujuan />
             </BendaharaPage>
           }
         />
@@ -142,22 +132,18 @@ export default function App() {
         />
         <Route path="/siswa-ortu/profile" element={<ProfileSiswaOrtu />} />
       </Routes>
-    </BrowserRouter>
-  );
-}
       <div style={{ display: "flex" }}>
         <SidebarBendahara />
         <div className="content-wrapper">
           <Routes>
             <Route path="/bendahara/" element={<Dashboard />} />
             <Route path="/bendahara/dashboard" element={<Dashboard />} />
-            {/* <Route path="/bendahara/persetujuan" element={<Persetujuan />} /> */}
             <Route path="/bendahara/dana" element={<Dana />} />
             <Route path="/bendahara/rka" element={<RKA />} />
             <Route path="/bendahara/bku" element={<BKU />} />
             <Route path="/bendahara/bkm" element={<BKM />} />
             <Route path="/bendahara/bkk" element={<BKK />} />
-            <Route path="/bendahara/tagihan" element={<Tagihan />} />
+            {/* <Route path="/bendahara/tagihan" element={<Tagihan />} /> */}
             <Route path="/bendahara/tarif" element={<Tarif />} />
             <Route path="/bendahara/laporan" element={<Laporan />} />
             <Route path="/bendahara/master/coa" element={<MasterCOA />} />
