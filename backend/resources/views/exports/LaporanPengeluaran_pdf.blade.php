386fa147578f088da0355116b98b3ec7c7e57f85
larasmarut2608 <larasmarut2608@gmail.com>
2026-04-16 17:41:15 +0700

    ubah code pada laporan

--- backend/resources/views/exports/LaporanPengeluaran_pdf.blade.php
+++ backend/resources/views/exports/LaporanPengeluaran_pdf.blade.php
@@ -41,7 +41,6 @@
             font-weight: bold;
         }
 
-        /* WATERMARK (LEBIH HALUS & TENGAH) */
         .watermark {
             position: absolute;
             top: 50%;
@@ -123,7 +122,7 @@
                 <br>
                 <b>By: Bendahara</b>
                 <br><br><br><br><br>
-                <b>{{ Auth::user()->name ?? '.......................' }}</b>
+                <b>{{ Auth::user()->name ?? }}</b>
             </td>
         </tr>
     </table>
