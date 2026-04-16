24a45869c4492d220c5bff66fd04da4694db15af
GianTechWizard <giannkolebaa@gmail.com>
2026-04-16 23:35:38 +0700

    ubah

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,18 +12,18 @@ import BKK from "./pages/bendahara/BKK";
 import Tagihan from "./pages/bendahara/Tagihan";
 import Tarif from "./pages/bendahara/Tarif";
 import Laporan from "./pages/bendahara/Laporan";
-import Verifikasi from "./pages/bendahara/Verifikasi"
-import Penerimaan from "./pages/bendahara/Penerimaan"
-import MasterCOA from "./pages/bendahara/MasterCOA"
-import MasterKegiatan from "./pages/bendahara/MasterKegiatan"
-import MasterTahunAnggaran from "./pages/bendahara/MasterTahunAnggaran"
-import MasterTahunAkademik from "./pages/bendahara/MasterTahunAkademik"
-import MasterSumberDana from "./pages/bendahara/MasterSumberDana"
-import MasterRefPenerimaan from "./pages/bendahara/MasterRefPenerimaan"
-import MasterTarif from "./pages/bendahara/MasterTarif"
-import MasterJenisTarif from "./pages/bendahara/MasterJenisTarif"
-import MasterJenisPembayaran from "./pages/bendahara/MasterJenisPembayaran"
-import Log from "./pages/bendahara/LogAktivitas"
+import Verifikasi from "./pages/bendahara/Verifikasi";
+import Penerimaan from "./pages/bendahara/Penerimaan";
+import MasterCOA from "./pages/bendahara/MasterCOA";
+import MasterKegiatan from "./pages/bendahara/MasterKegiatan";
+import MasterTahunAnggaran from "./pages/bendahara/MasterTahunAnggaran";
+import MasterTahunAkademik from "./pages/bendahara/MasterTahunAkademik";
+import MasterSumberDana from "./pages/bendahara/MasterSumberDana";
+import MasterRefPenerimaan from "./pages/bendahara/MasterRefPenerimaan";
+import MasterTarif from "./pages/bendahara/MasterTarif";
+import MasterJenisTarif from "./pages/bendahara/MasterJenisTarif";
+import MasterJenisPembayaran from "./pages/bendahara/MasterJenisPembayaran";
+import Log from "./pages/bendahara/LogAktivitas";
 import "./index.css";
 
 export default function App() {
@@ -45,14 +45,35 @@ export default function App() {
             <Route path="/bendahara/tarif" element={<Tarif />} />
             <Route path="/bendahara/laporan" element={<Laporan />} />
             <Route path="/bendahara/master/coa" element={<MasterCOA />} />
-            <Route path="/bendahara/master/kegiatan" element={<MasterKegiatan />} />
-            <Route path="/bendahara/master/tahun-anggaran" element={<MasterTahunAnggaran />} />
-            <Route path="/bendahara/master/tahun-akademik" element={<MasterTahunAkademik />} />
-            <Route path="/bendahara/master/sumber-dana" element={<MasterSumberDana />} />
-            <Route path="/bendahara/master/ref-penerimaan" element={<MasterRefPenerimaan />} />
+            <Route
+              path="/bendahara/master/kegiatan"
+              element={<MasterKegiatan />}
+            />
+            <Route
+              path="/bendahara/master/tahun-anggaran"
+              element={<MasterTahunAnggaran />}
+            />
+            <Route
+              path="/bendahara/master/tahun-akademik"
+              element={<MasterTahunAkademik />}
+            />
+            <Route
+              path="/bendahara/master/sumber-dana"
+              element={<MasterSumberDana />}
+            />
+            <Route
+              path="/bendahara/master/ref-penerimaan"
+              element={<MasterRefPenerimaan />}
+            />
             <Route path="/bendahara/master/tarif" element={<MasterTarif />} />
-            <Route path="/bendahara/master/jenis-tarif" element={<MasterJenisTarif />} />
-            <Route path="/bendahara/master/jenis-pembayaran" element={<MasterJenisPembayaran />} />
+            <Route
+              path="/bendahara/master/jenis-tarif"
+              element={<MasterJenisTarif />}
+            />
+            <Route
+              path="/bendahara/master/jenis-pembayaran"
+              element={<MasterJenisPembayaran />}
+            />
             <Route path="/bendahara/penerimaan" element={<Penerimaan />} />
             <Route path="/bendahara/verifikasi" element={<Verifikasi />} />
             <Route path="/bendahara/log" element={<Log />} />
