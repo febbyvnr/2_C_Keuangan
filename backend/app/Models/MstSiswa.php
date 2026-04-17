f123c0edd118199aa9945bfb5c9d6fec6576cf12
Febby Venera <230712301@students.uajy.ac.id>
2026-04-17 10:12:38 +0700

    FIX: error fetch data page Verifikasi & TagihanSiswa
    JANGAN DIHAPUS LAGI YA BACKEND MSTSISWANYA

--- backend/app/Models/MstSiswa.php
+++ backend/app/Models/MstSiswa.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class MstSiswa extends Model
 {
-    //
+    protected $table = 'mst_siswa';
+    protected $primaryKey = 'ID_SISWA_TETAP';
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $keyType = 'int';
 }
