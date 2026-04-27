3d87c94672591caea050e5eb92ddbe63c3a6748b
Febby Venera <230712301@students.uajy.ac.id>
2026-04-27 11:32:04 +0700

    hapus file tdk terpakai

--- frontend/src/pages/waka/approve/PencairanDanaPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function PencairanDanaPage() {
-    return (
-        <div>
-            <h2>Pencairan Dana Page</h2>
-            <p>Isi nanti di sini...</p>
-        </div>
-    );
-}
--- frontend/src/pages/waka/approve/RKAPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function RKAPage({ setHasPending }) {
-    return (
-        <div>
-            <h2>RKA Page</h2>
-            <p>Isi nanti di sini...</p>
-        </div>
-    );
-}
