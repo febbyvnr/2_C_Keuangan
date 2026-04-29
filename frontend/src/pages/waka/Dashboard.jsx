12a9679df328e86ee5e407963c262783f5198e55
GianTechWizard <giannkolebaa@gmail.com>
2026-04-29 20:01:57 +0700

    tambah tahun

--- frontend/src/pages/waka/Dashboard.jsx
+++ frontend/src/pages/waka/Dashboard.jsx
@@ -175,6 +175,7 @@ export default function Dashboard() {
                 <option value="all">Semua</option>
                 <option value="2024">2024</option>
                 <option value="2025">2025</option>
+                <option value="2026">2026</option>
               </select>
             </div>
 
