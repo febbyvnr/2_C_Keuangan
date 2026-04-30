62153cd9ff58d5b11b6df5aad55d385f573e25d8
230712318 <230712318@students.uajy.ac.id>
2026-04-30 14:10:45 +0700

    menambahkan kembali kode

--- backend/app/Exports/RefJenisPembayaranExport.php
+++ backend/app/Exports/RefJenisPembayaranExport.php
@@ -122,7 +122,7 @@ class RefJenisPembayaranExport implements WithEvents
                 $footerRow = $endData + 3;
 
                 $role = $this->role ?: 'Bendahara';
-                $nama = 'Rina Putri, S.E.';
+                $nama = $this->nama ?: 'Rina Putri, S.E.';
                 $nip = $this->nip ?: '-';
 
                 // TANGGAL
