dbe4be8fe8c36a8fd97ebe46c44426b017591f91
230712318 <230712318@students.uajy.ac.id>
2026-05-13 19:52:41 +0700

    commit sebelum pengujian masuk tidaknya hasil tagihan

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App />
     </StrictMode>,
 )
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <App />
+
+    <ToastContainer
+      position="top-right"
+      autoClose={4000}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick
+      pauseOnHover
+      draggable
+      theme="colored"
+    />
+  </React.StrictMode>
+);
